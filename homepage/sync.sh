e32d6a0d3eee412086dbbc2481bd17a3de2cee30
totemtang <totem825@gmail.com>
2023-06-07 21:10:21 +0800

    Done with remodeling the homepage

--- homepage/sync.sh
+++ homepage/sync.sh
@@ -6,11 +6,11 @@ remote=watson.millennium.berkeley.edu
 remote_dir=/home/eecs/totemtang/public_html
 
 #scp icon.png $remote:$remote_dir/
-scp index.html $remote:$remote_dir/
+scp *.html $remote:$remote_dir/
 #scp CV/* $remote:$remote_dir/CV/
-#scp paper/* $remote:$remote_dir/paper/
+scp paper/* $remote:$remote_dir/paper/
 #scp css/* $remote:$remote_dir/css/
 #scp img/* $remote:$remote_dir/img/
 #scp fonts/* $remote:$remote_dir/fonts/
-#scp js/* $remote:$remote_dir/js/
+scp js/w3.js $remote:$remote_dir/js/
 
