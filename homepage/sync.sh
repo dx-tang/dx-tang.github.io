e4ccd6ca2d5094b3418ab9be90425b77245d152a
totemtang <totem825@gmail.com>
2023-06-07 21:59:26 +0800

    small edits

--- homepage/sync.sh
+++ homepage/sync.sh
@@ -6,8 +6,8 @@ remote=watson.millennium.berkeley.edu
 remote_dir=/home/eecs/totemtang/public_html
 
 #scp icon.png $remote:$remote_dir/
-#scp *.html $remote:$remote_dir/
-scp CV/* $remote:$remote_dir/CV/
+scp *.html $remote:$remote_dir/
+#scp CV/* $remote:$remote_dir/CV/
 #scp paper/* $remote:$remote_dir/paper/
 #scp css/* $remote:$remote_dir/css/
 #scp img/* $remote:$remote_dir/img/
