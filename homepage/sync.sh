02dbd6caee259d478aab1dcf158c3e4bf250770b
totemtang <totem825@gmail.com>
2023-09-22 22:22:10 -0500

    adding the recruitment posting

--- homepage/sync.sh
+++ homepage/sync.sh
@@ -7,10 +7,10 @@ remote_dir=/home/eecs/totemtang/public_html
 
 #scp icon.png $remote:$remote_dir/
 scp *.html $remote:$remote_dir/
-scp CV/* $remote:$remote_dir/CV/
+#scp CV/* $remote:$remote_dir/CV/
 #scp paper/* $remote:$remote_dir/paper/
 #scp css/* $remote:$remote_dir/css/
-#scp img/* $remote:$remote_dir/img/
+scp img/* $remote:$remote_dir/img/
 #scp fonts/* $remote:$remote_dir/fonts/
 #scp js/w3.js $remote:$remote_dir/js/
 
